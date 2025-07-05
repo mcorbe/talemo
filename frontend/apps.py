c74083a054fcb4d3468ab4ad75cd0010a0087aad
mco <this@that.com>
2025-07-05 13:36:39 +0200

    Add frontend app and tenant-specific configurations: implement initial setup for the `frontend` app, shared and tenant schema migrations script, public-specific URLs, and app configurations.

---
+++ frontend/apps.py
@@ -0,0 +1,10 @@
+"""
+Frontend app configuration.
+"""
+from django.apps import AppConfig
+
+
+class FrontendConfig(AppConfig):
+    """Frontend app configuration."""
+    name = 'frontend'
+    verbose_name = 'Frontend'
